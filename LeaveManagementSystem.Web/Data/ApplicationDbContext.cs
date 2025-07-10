9016aa5b411d60ad475e4b35c91fd6d1b1bd87c2
moatazbadr <zezobadr988@gmail.com>
2025-07-10 16:59:32 +0300

    Adding LeaveTypes CRUD and update UI

--- LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem.Web/Data/ApplicationDbContext.cs
@@ -9,5 +9,7 @@ namespace LeaveManagementSystem.Web.Data
             : base(options)
         {
         }
+
+        public DbSet<LeaveType> leaveTypes { get; set; }
     }
 }
---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        public string Name { get; set; }
+        public int Days { get; set; }
+    }
+}
